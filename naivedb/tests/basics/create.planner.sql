f18464731a96fa38c2456907bb9fb478f2ce74fc
Alex Chi <iskyzh@gmail.com>
2024-12-07 14:23:52 -0500

    add tasks
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- naivedb/tests/basics/create.planner.sql
+++ naivedb/tests/basics/create.planner.sql
@@ -2,6 +2,13 @@
 CREATE TABLE t1(v1 int);
 
 /*
+=== logical
+I'm a naive db, so I don't now how to process
+CREATE TABLE t1(v1 int);
 
+
+=== physical
+I'm a naive db, so I don't now how to process
+CREATE TABLE t1(v1 int);
 */
 
--- naivedb/tests/basics/select.planner.sql
+++ naivedb/tests/basics/select.planner.sql
@@ -2,6 +2,15 @@
 SELECT * FROM t1;
 
 /*
+=== logical
+I'm a naive db, so I don't now how to process
+CREATE TABLE t1(v1 int);
+SELECT * FROM t1;
 
+
+=== physical
+I'm a naive db, so I don't now how to process
+CREATE TABLE t1(v1 int);
+SELECT * FROM t1;
 */
 
